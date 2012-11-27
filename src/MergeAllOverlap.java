a7273fab31caccb82d6b88ece0c4c40519d692be
huangyingw@linux_programming <huangyingw@gmail.com>
2012-11-26 23:53:15 -0800

    n

--- src/MergeAllOverlap.java
+++ src/MergeAllOverlap.java
@@ -1,19 +1,3 @@
-/*
-Given a collection of intervals, merge all overlapping intervals.
-
-For example,
-Given [1,3],[2,6],[8,10],[15,18],
-return [1,6],[8,10],[15,18].
- */
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
