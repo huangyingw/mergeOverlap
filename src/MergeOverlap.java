823247fa2c78425c83ffa8bb244f760f627ff69e
huangyingw@linux_programming <huangyingw@gmail.com>
2012-11-26 22:36:44 -0800

    n

--- src/MergeOverlap.java
+++ src/MergeOverlap.java
@@ -1,92 +1,81 @@
 import java.util.List;
 
-
 public class MergeOverlap<E> {
 
+	class Interval {
+		public Interval(int s, int e) {
+			start = s;
+			end = e;
+		}
 
-	class Interval
-	{
-	  public Interval(int s, int e)  {
-		  start=s;
-		  end=e;
-	  }
-	  int start;
-	  int end;
+		int start;
+		int end;
 	};
 
+	List<Interval> merge_overlaps(List<Interval> intervals, Interval another) {
+		List<Interval> result;
 
-	List<Interval> merge_overlaps(List<Interval> intervals, Interval another)
-	{
-	  List<Interval> result; 
+		boolean merge_finished = false;
 
-	  boolean merge_finished = false;
-	  
-	  
-	  for(int it=0;it<intervals.size();it++){
-		  Interval inter=intervals.get(it);
-		    if ( inter.start < another.end && another.start < inter.end){
-			      another.start = (inter.start < another.start) ? inter.start : another.start;
-			      another.end = (inter.end < another.end) ? another.end : inter.end;
-			      continue;
-			    }
-			                                           
-			    if (inter.start > another.end){  
-			      result.add(another); 
-			      merge_finished = true;
-			    }
+		for (int it = 0; it < intervals.size(); it++) {
+			Interval inter = intervals.get(it);
+			if (inter.start < another.end && another.start < inter.end) {
+				another.start = (inter.start < another.start) ? inter.start
+						: another.start;
+				another.end = (inter.end < another.end) ? another.end
+						: inter.end;
+				continue;
+			}
 
-			    result.add(inter);
-	  }
+			if (inter.start > another.end) {
+				result.add(another);
+				merge_finished = true;
+			}
 
-	  if (! merge_finished) result.add(another);
+			result.add(inter);
+		}
 
-	  return result;
+		if (!merge_finished)
+			result.add(another);
+
+		return result;
 	}
 
-
-	void print_intervals(List<Interval> & merged)
-	{
-	  for (int i = 0; i < merged.size(); i++){
-	    cout << "(" << merged[i].start <<", " << merged[i].end<<") ";
-	  }
-	  cout << endl;
+	void print_intervals(List<Interval> merged) {
+		// for (int i = 0; i < merged.size(); i++){
+		// cout << "(" << merged[i].start <<", " << merged[i].end<<") ";
+		// }
+		// cout << endl;
 	}
 
+	void test1() {
+		List<Interval> intervals;
+		intervals.add(new Interval(1, 4));
+		intervals.add(new Interval(6, 10));
+		intervals.add(new Interval(14, 19));
 
-	void test1()
-	{
-	  List<Interval> intervals;
-	  intervals.add(new Interval(1,4) );
-	  intervals.add(new Interval(6,10) );
-	  intervals.add(new Interval(14,19) );
-
-	  Interval another(13, 17);
-	  List<Interval> merged = merge_overlaps(intervals, another);
-	  print_intervals(merged);
+		Interval another = new Interval(13, 17);
+		List<Interval> merged = merge_overlaps(intervals, another);
+		print_intervals(merged);
 	}
 
-	void test2()
-	{
-	  List<Interval> intervals;
-	  intervals.add( new Interval(1,5));
-	  intervals.add( new Interval(6,15));
-	  intervals.add( new Interval(20,21));
-	  intervals.add( new Interval(23,26));
-	  intervals.add(new  Interval(27,30));
-	  intervals.add(new  Interval(35,40));
-	  
-	  Interval another(14,33);
-	  List<Interval> merged = merge_overlaps(intervals, another);
-	  print_intervals(merged);
+	void test2() {
+		List<Interval> intervals;
+		intervals.add(new Interval(1, 5));
+		intervals.add(new Interval(6, 15));
+		intervals.add(new Interval(20, 21));
+		intervals.add(new Interval(23, 26));
+		intervals.add(new Interval(27, 30));
+		intervals.add(new Interval(35, 40));
+
+		Interval another = new Interval(14, 33);
+		List<Interval> merged = merge_overlaps(intervals, another);
+		print_intervals(merged);
 	}
 
-	int main()
-	{
-	  test1();
-	  test2();
-	  return 0;
+	int main() {
+		test1();
+		test2();
+		return 0;
 	}
-
-
-
 }
